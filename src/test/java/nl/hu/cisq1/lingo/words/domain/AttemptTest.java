d8454eec60745a6815952fa5f7bd37796d048efe
rafaeldatarock <rafaeldatarock@gmail.com>
2021-11-30 20:14:12 +0100

    Accidentally pushed testcases commented

--- src/test/java/nl/hu/cisq1/lingo/words/domain/AttemptTest.java
+++ src/test/java/nl/hu/cisq1/lingo/words/domain/AttemptTest.java
@@ -18,14 +18,14 @@ class AttemptTest {
 
     public static Stream<Arguments> guessExamples() {
         return Stream.of(
-            // Arguments.of(new Word("braam"), "braam", List.of(CORRECT, CORRECT, CORRECT, CORRECT, CORRECT)),
-            // Arguments.of(new Word("aangaan"), "koelrib", List.of(ABSENT, ABSENT, ABSENT, ABSENT, ABSENT, ABSENT, ABSENT)),
-            // Arguments.of(new Word("braam"), "braad", List.of(CORRECT, CORRECT, CORRECT, CORRECT, ABSENT)),
-            // Arguments.of(new Word("braam"), "vroom", List.of(ABSENT, CORRECT, ABSENT, ABSENT, CORRECT)),
+            Arguments.of(new Word("braam"), "braam", List.of(CORRECT, CORRECT, CORRECT, CORRECT, CORRECT)),
+            Arguments.of(new Word("aangaan"), "koelrib", List.of(ABSENT, ABSENT, ABSENT, ABSENT, ABSENT, ABSENT, ABSENT)),
+            Arguments.of(new Word("braam"), "braad", List.of(CORRECT, CORRECT, CORRECT, CORRECT, ABSENT)),
+            Arguments.of(new Word("braam"), "vroom", List.of(ABSENT, CORRECT, ABSENT, ABSENT, CORRECT)),
             Arguments.of(new Word("braam"), "baard", List.of(CORRECT, PRESENT, CORRECT, PRESENT, ABSENT)),
-            // Arguments.of(new Word("baard"), "braam", List.of(CORRECT, PRESENT, CORRECT, PRESENT, ABSENT)),
-            // Arguments.of(new Word("baard"), "bedde", List.of(CORRECT, ABSENT, PRESENT, ABSENT, ABSENT)),
-            // Arguments.of(new Word("baard"), "bonje", List.of(CORRECT, ABSENT, ABSENT, ABSENT, ABSENT)),
+            Arguments.of(new Word("baard"), "braam", List.of(CORRECT, PRESENT, CORRECT, PRESENT, ABSENT)),
+            Arguments.of(new Word("baard"), "bedde", List.of(CORRECT, ABSENT, PRESENT, ABSENT, ABSENT)),
+            Arguments.of(new Word("baard"), "bonje", List.of(CORRECT, ABSENT, ABSENT, ABSENT, ABSENT)),
             Arguments.of(new Word("baard"), "barst", List.of(CORRECT, CORRECT, PRESENT, ABSENT, ABSENT))
         );
     }
