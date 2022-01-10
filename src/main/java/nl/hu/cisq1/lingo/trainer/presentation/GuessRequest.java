5c15257f041f7f4331f6dbc73d25d5749e8ba495
rafaeldatarock <rafaeldatarock@gmail.com>
2022-01-10 14:15:30 +0100

    Analysis done

--- src/main/java/nl/hu/cisq1/lingo/trainer/presentation/GuessRequest.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/presentation/GuessRequest.java
@@ -1,6 +1,7 @@
 package nl.hu.cisq1.lingo.trainer.presentation;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import nl.hu.cisq1.lingo.trainer.domain.Generated;
@@ -8,6 +9,7 @@ import nl.hu.cisq1.lingo.trainer.domain.Generated;
 class GuessRequest {
     @NotNull
     @Size(min = 5, max = 7, message = "Guess should be between 5 and 7 letters.")
+    @Pattern(regexp = "^[a-zA-Z]+$")
     private String guess;
 
     @Generated
